352464029bfc8238c283dd5e5f05bbd706092657
jin <jinzhaocnfr@gmail.com>
2023-12-10 09:24:35 +0100

    added front-page content with a  random image header strcuture only no css yet

--- front-page.php
+++ front-page.php
@@ -1,4 +1,25 @@
 <?php get_header();?>
 
+<?php
+$args = array(  
+    'post_type' => 'photo',
+    'post_status' => 'publish',
+    'posts_per_page' => 1, 
+    'orderby' => 'rand', 
+);
+
+$loop = new WP_Query( $args ); 
+    
+while ( $loop->have_posts() ) : $loop->the_post(); ?>
+
+<a href="<?php the_permalink() ?>"><?php the_content(); ?></a>
+
+
+
+<?php endwhile;
+
+ wp_reset_postdata();
+?>
+
 
 <?php get_footer();
